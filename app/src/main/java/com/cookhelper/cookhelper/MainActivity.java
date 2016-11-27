648caefbc7c2bff99a8ab8c73ec074d80c54095f
ReynaDoer <rdoer094@uottawa.ca>
2016-11-27 16:50:48 -0500

    Test

--- app/src/main/java/com/cookhelper/cookhelper/MainActivity.java
+++ app/src/main/java/com/cookhelper/cookhelper/MainActivity.java
@@ -3,9 +3,7 @@ package com.cookhelper.cookhelper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View;
-import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +13,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /*Button btnAdd = (Button)findViewById(R.id.Add);
 
-    btnAdd.setOnClickListener(new View.OnClickListener() {*/
-        public void btnAddClick(View view){
-            Intent startAdd = new Intent(this, AddRecipe.class);
-            startActivity(startAdd);
-        }
+    public void btnAddClick(View view){
+        Intent startAdd = new Intent(this, AddRecipe.class);
+        startActivity(startAdd);
+    }
 
 
     //open help screen on Help Button click
